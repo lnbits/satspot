d8957ac9a66deb895076d8cc728da7eef655e47f
Arc <ben@arc.wales>
2024-11-05 16:03:09 +0000

    Revert "ooo, just discovered /api/v1/ws/pay_hash"
    This reverts commit 202b555ea7cba7f0ee2797fb837613b6c1961f17.

--- __init__.py
+++ __init__.py
@@ -1,8 +1,11 @@
 import asyncio
 
 from fastapi import APIRouter
+from lnbits.tasks import create_permanent_unique_task
+from loguru import logger
 
 from .crud import db
+from .tasks import wait_for_paid_invoices
 from .views import satspot_generic_router
 from .views_api import satspot_api_router
 
@@ -16,5 +19,21 @@ satspot_static_files = [
         "name": "satspot_static",
     }
 ]
+
+scheduled_tasks: list[asyncio.Task] = []
+
+
+def satspot_stop():
+    for task in scheduled_tasks:
+        try:
+            task.cancel()
+        except Exception as ex:
+            logger.warning(ex)
+
+
+def satspot_start():
+    task = create_permanent_unique_task("ext_satspot", wait_for_paid_invoices)
+    scheduled_tasks.append(task)
+
 
 __all__ = ["db", "satspot_ext", "satspot_static_files"]
---
+++ tasks.py
@@ -0,0 +1,63 @@
+import asyncio
+from datetime import datetime
+from lnbits.core.models import Payment
+from lnbits.core.services import pay_invoice, websocket_updater
+from lnbits.tasks import register_invoice_listener
+
+from .crud import (
+    get_satspot,
+    update_satspot,
+)
+from .helpers import get_pr, calculate_winner
+
+
+async def wait_for_paid_invoices():
+    invoice_queue = asyncio.Queue()
+    register_invoice_listener(invoice_queue, "ext_satspot")
+
+    while True:
+        payment = await invoice_queue.get()
+        await on_invoice_paid(payment)
+
+
+async def on_invoice_paid(payment: Payment) -> None:
+    if payment.extra.get("tag") == "satspot":
+        ln_address = payment.extra["ln_address"]
+        satspot_id = payment.extra["satspot_id"]
+        # fetch details
+        satspot = await get_satspot(satspot_id)
+        if not satspot:
+            return
+        # Check they are not trying to scam the system.
+        if (payment.amount / 1000) != satspot.buy_in:
+            return
+        await calculate_winner(satspot)
+        # If player joins late send a refund
+        if int(datetime.now().timestamp()) > satspot.closing_date:
+            satspot.completed = True
+            await update_satspot(satspot)
+
+            # Calculate the haircut amount
+            haircut_amount = satspot.buy_in * (satspot.haircut / 100)
+            # Calculate the refund amount
+            max_sat = int(satspot.buy_in - haircut_amount)
+            pr = await get_pr(ln_address, max_sat)
+            if not pr:
+                return
+            await pay_invoice(
+                wallet_id=satspot.wallet,
+                payment_request=pr,
+                max_sat=max_sat,
+                description="Refund. Satspot game was full.",
+            )
+            await websocket_updater(payment.payment_hash, "refund")
+            await calculate_winner(satspot)
+            return
+
+        # Add the player to the game.
+        if satspot.players == "":
+            satspot.players = ln_address
+        else:
+            satspot.players = f"{satspot.players},{ln_address}"
+        await update_satspot(satspot)
+        await websocket_updater(payment.payment_hash, "joined")
